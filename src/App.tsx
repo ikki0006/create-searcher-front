9a8dfdfcc6e82fa31e422aeb3547b228351d672f
ikki0006 <ikki0006@gmail.com>
2023-03-24 18:01:28 +0000

    ディレクトリ構成完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/images/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
